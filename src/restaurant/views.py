ebc96d80abcb28e7855737813fc8ec584c85249e
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-23 16:08:38 +0800

    viewing data from models to templates

--- src/restaurant/views.py
+++ src/restaurant/views.py
@@ -1,39 +1,74 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import random
+#import random
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from .models import RestaurantLocation
 # Create your views here.
 
-class HomeView(TemplateView):
-	template_name = 'home.html'
+def restaurant_listview(request):
+	template_name = 'restaurant/restaurant-list.html'
+	ResLoc = RestaurantLocation.objects.all()
+	context = {
+		"object_list" : ResLoc
+	}
+	return render(request, template_name, context)
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(HomeView, self).get_context_data(*args, **kwargs)
-		num = None
-		some_list = [
-			random.randint(0, 1000000),
-			random.randint(0, 1000000),
-			random.randint(0, 1000000),
-		]
-		condition_bool_item = True
-		if condition_bool_item:
-			num = random.randint(0, 1000000)
-		context = {
-			"num" : num,
-			"some_list": some_list
-		}
-		return context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class HomeView(TemplateView):
+# 	template_name = 'home.html'
+
+# 	def get_context_data(self, *args, **kwargs):
+# 		context = super(HomeView, self).get_context_data(*args, **kwargs)
+# 		num = None
+# 		some_list = [
+# 			random.randint(0, 1000000),
+# 			random.randint(0, 1000000),
+# 			random.randint(0, 1000000),
+# 		]
+# 		condition_bool_item = True
+# 		if condition_bool_item:
+# 			num = random.randint(0, 1000000)
+# 		context = {
+# 			"num" : num,
+# 			"some_list": some_list
+# 		}
+# 		return context
 
 # class AboutView(TemplateView):
 # 	template_name = 'about.html'
 
 # class ContactView(TemplateView):
 # 	template_name = 'contact.html'
-	
+
 #function based view
 
 # def home_old(request):
